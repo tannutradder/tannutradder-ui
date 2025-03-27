cd97e9bf04f32f277d635911ef039b5e1155d511
Ishwar Patidar <ishwarpatidar015@gmail.com>
2025-03-27 22:46:23 +0530

    books add

--- src/Utility/ConstantData/DummyData.ts
+++ src/Utility/ConstantData/DummyData.ts
@@ -1,10 +1,10 @@
 export const courses = [
   {
-    image: 'basic_advance.jpeg',
+    image: 'basic_advance2.jpeg',
     title: 'Basic to Advance course',
     author: '',
-    oldPrice: '₹17,000.00',
-    newPrice: '₹11999.00'
+    oldPrice: '₹35,000.00',
+    newPrice: '₹28000.00'
   },
   {
     image: 'candle_stick.jpeg',
@@ -19,5 +19,36 @@ export const courses = [
     author: '',
     oldPrice: '₹1500.00',
     newPrice: '₹999.00'
+  }
+];
+
+export const books = [
+  {
+    image: 'assets/books/book3.jpeg',
+    title: 'Trading Rules',
+    oldPrice: '₹799.00',
+    newPrice: '₹500.00',
+    description: 'All Important Trading rules are available in this book to be a successful and profitable Trader....'
+  },
+  {
+    image: 'assets/books/book2.jpeg',
+    title: 'Jese Chaho wese Dimag ko control karo',
+    oldPrice: '₹1000.00',
+    newPrice: '₹500.00',
+    description: ''
+  },
+  {
+    image: 'assets/books/book6.jpeg',
+    title: 'Price Action Book',
+    oldPrice: '₹1000.00',
+    newPrice: '₹500.00',
+    description: 'All information about Price Action.'
+  },
+  {
+    image: 'assets/books/book5.jpeg',
+    title: ' Trade through SMC Book',
+    oldPrice: '₹1000.00',
+    newPrice: '₹500.00',
+    description: 'all concepts related to SMC.'
   }
 ];
